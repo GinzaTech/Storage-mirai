95c69c71d8e739885fbdbc425b801b9b67b30430
Vũ Nguyễn Thành Tâm <rfechinonguyen@gmail.com>
2021-03-14 19:31:55 +0700

    update method

--- install.sh
+++ install.sh
@@ -6,40 +6,36 @@ cat << "EOF"
  | |  | | | | | |    | (_| | | |    | |  | | | | \__ \ | |_  | (_| | | | | |
  |_|  |_| |_| |_|     \__,_| |_|   |___| |_| |_| |___/  \__|  \__,_| |_| |_|
 
-special thanks to:
- Trần Xuân Thanh(ging-dev) for termux-manager - https://github.com/ging-dev/termux-manager
- Phạm Quang Huy(huybopbi) for contribute this method :3
+Special thanks to:
+  Trần Xuân Thanh(ging-dev) for termux-manager.
+  Phạm Quang Huy(huybopbi) for contribute this method.
 
+===============================================
+Installation file made by CatalizCS with love <3
+GITHUB: https://github.com/CatalizCS/miraiv2
+MIRAIPROJECT LÀ MÃ NGUỒN MỞ, HOÀN TOÀN KHÔNG ĐƯỢC BUÔN BÁN Ở BẤT CỨ NƠI NÀO, HÃY CẨN THẬN!
+LƯU Ý: SHELL SCRIPT NÀY CHỈ DÀNH CHO TERMUX!
+Để hệ thống hoạt động tối ưu, ít nhất dung lượng máy phải trống khoảng 1-2GB.
+Nhấn enter để bắt đầu tiến hành cài đặt.
 
 EOF
-
-echo "==============================================="
-echo "                MIRAI INSTALL                  "
-echo "==============================================="
-echo "==============================================="
-echo "Install file make by CatalizCS with love <3"
-echo "GITHUB: https://github.com/CatalizCS/miraiv2"
-echo "MIRAIPROJECT LÀ MÃ NGUỒN MỞ, HOÀN TOÀN KHÔNG ĐƯỢC BUÔN BÁN Ở BẤT CỨ NƠI NÀO, HÃY CẨN THẬN!!"
-echo "LƯU Ý: SHELL SCRIPT NÀY CHỈ DÀNH CHO TERMUX!"
-echo "Để hệ thống hoạt động tối ưu, ít nhất dung lượng máy phải trống khoảng 1-2gb"
-echo "Nhấn enter để bắt đầu tiến hành cài đặt..."
 read
 
-echo "[!] Tiến hành cập nhật, cài đặt các gói library còn thiếu..."
-pkg install libpixman libcairo pango xorgproto wget php libjpeg-turbo-progs libjpeg-turbo-static libjpeg-turbo librsvg librsvg-static nodejs git python -y
+echo "==============================================="
+echo "[!] Tiến hành cài đặt các gói library còn thiếu [!]"
+pkg install -y libpixman libcairo pango xorgproto php libjpeg-turbo-progs libjpeg-turbo-static libjpeg-turbo librsvg librsvg-static nodejs git python wget
 git clone https://github.com/catalizCS/miraiv2
 cd miraiv2
-echo "[!] Đã cài đặt source code thành công, tiến hành cài các gói cần thiết..."
-npm install
-echo "[!] Đã cài các gói cần thiết thành công, tiến hành cài đặt file manager..."
-wget https://raw.githubusercontent.com/catalizcs/storage-data/master/install-fmanager.sh
-bash install-fmanager.sh
-echo "[!] Đã cài đặt file manager hoàn tất!"
-
+echo "[!] Đã tải source code thành công, tiến hành cài các gói cần thiết [!]"
+npm i
+echo "[!] Đã cài các gói cần thiết thành công, tiến hành cài đặt file manager [!]"
+bash <(wget -qO- https://raw.githubusercontent.com/catalizcs/storage-data/master/install-fmanager.sh)
+echo "[!] Đã cài đặt file manager thành công [!]"
 
 echo "==============================================="
-echo "Đã cài đặt thành công, tiếp theo hãy mở manager và edit file config.json, thêm appstate"
-echo "để mở file manager, bạn chỉ cần copy và paste 'cd && manager'(bỏ dấu ') là có thể mở file manager"
+echo "Cài đặt hoàn tất! Hãy sửa file config.json, thêm appstate ở cửa sổ sau."
 echo "Toàn bộ thư mục của bot được lưu tại '/home/miraiv2'"
-echo "Nhấn enter để thoát tiến trình cài đặt..."
+echo "Nhấn enter để mở file manager."
 read
+
+manager
