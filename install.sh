623ac9b92f8fd3159f5a2dddb83f17eb36fb3ab7
Luna <96804285+GinzaTech@users.noreply.github.com>
2023-07-25 21:43:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ read
 
 echo "==============================================="
 echo "[!] Tiến hành cài đặt các gói library còn thiếu [!]"
-pkg install -y libpixman libcairo pango xorgproto php libjpeg-turbo-progs libjpeg-turbo-static libjpeg-turbo librsvg librsvg-static nodejs git python wget
+pkg install -y libpixman libcairo pango xorgproto php libjpeg-turbo-progs libjpeg-turbo-static libjpeg-turbo librsvg nodejs git python wget
 
 git clone https://github.com/miraipr0ject/miraiv2
 cd miraiv2
